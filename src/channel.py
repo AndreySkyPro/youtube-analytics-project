d0f467137d4f32d4f9d0dc9c067f416904231ecf
Andrey <bmw_740@mail.ru>
2023-05-25 22:55:51 +0500

    competed task 3

--- src/channel.py
+++ src/channel.py
@@ -27,6 +27,7 @@ class Channel:
     @property
     def channel_id(self):
         return self._channel_id
+
     @channel_id.setter
     def channel_id(self, channel_id):
         if self._channel_id is None:
@@ -56,3 +57,27 @@ class Channel:
         }
         with open(filename, 'w') as f:
             json.dump(data, f, indent=2, ensure_ascii=False)
+
+    def __str__(self):
+        return f"{self.title} ({self.url})"
+
+    def __add__(self, other):
+        return int(self.subscriber_count) + int(other.subscriber_count)
+
+    def __sub__(self, other):
+        return int(self.subscriber_count) - int(other.subscriber_count)
+
+    def __gt__(self, other):
+        return int(self.subscriber_count) > int(other.subscriber_count)
+
+    def __ge__(self, other):
+        return int(self.subscriber_count) >= int(other.subscriber_count)
+
+    def __lt__(self, other):
+        return int(self.subscriber_count) < int(other.subscriber_count)
+
+    def __le__(self, other):
+        return int(self.subscriber_count) <= int(other.subscriber_count)
+
+    def __eq__(self, other):
+        return int(self.subscriber_count) == int(other.subscriber_count)
